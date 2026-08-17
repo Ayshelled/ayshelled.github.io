3093514c75b6f1b6f578cc78199f57dae67eccb7
Andrés Concha <acconcha@miuandes.cl>
2026-08-17 13:29:41 -0400

    Created script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const shows = [
+    {id:1, name: "Community", genre:"Sitcom", complete: 1 },
+    {id:2, name: "True Detective", genre:"Crime Drama", complete: 1 },
+    {id:3, name: "Malcom in the Middle", genre:"Sitcom", complete: 1},
+    {id:4, name: "Vinland Saga", genre: "Historical Drama", complete: 0},
+    {id:5, name: "Dungeon Meshi", genre: "Fantasy", complete: 0},
+    {id:6, name: "Game of Thrones", genre: "Dark Fantasy", complete: 1},
+    {id:7, name: "How I Met Your Mother", genre: "Sitcom", complete: 1}
+]
